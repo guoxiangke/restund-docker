e80b9ec251b43a54cd0a90043e2e5e97be96a7db
Matti Jokitulppo <melonmanchan@gmail.com>
2016-03-02 14:00:42 +0200

    Fix credentials

--- start-restund.sh
+++ start-restund.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sh /restund/util/genha1.sh $RESTUND_USERNAME $RESTUND_PASSWORD $RESTUND_REALM > /etc/restund.auth
+sh /restund/util/genha1.sh $RESTUND_USERNAME $RESTUND_REALM $RESTUND_PASSWORD > /etc/restund.auth
 
 LD_LIBRARY_PATH=/re/ restund /etc/restund.conf
 
